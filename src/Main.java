512e6339225a1d0e5379d80ef429270a7a1e30c6
Wairung Tiranalinvit <wairung3421@gmail.com>
2013-01-10 15:20:09 +0700

    Reset project

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,19 @@
-import org.jsoup.Connection;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
 
-import java.io.IOException;
-import java.util.Scanner;
+/**
+ *
+ * @author WaiToNZa
+ */
+public class main {
 
-public class Main {
-    public static void main(String[] args) throws IOException {
-        Scanner scn = new Scanner(System.in);
-        System.out.println("Please enter your student ID");
-        Connection.Response response = Jsoup.connect("https://login8.ku.ac.th/v2/").method(Connection.Method.GET).execute();
-        Document document = response.parse();
-        Element captcha = document.getElementsByTag("img").get(0);
-        String captchaHash = captcha.attr("src").toString().substring(13, 13+32);
-        System.out.println(captchaHash);
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
     }
 }

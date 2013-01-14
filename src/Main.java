f27fbc5c803651093a320edaa4fc563395e5c275
Athiwat Chunlakhan <athiwatc@gmail.com>
2013-01-14 14:15:12 +0700

    Now with auto login timmer
    It will now check if it needs to auto login

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 
 import Parser.LoginParser;
 import java.io.IOException;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 /**
  *
@@ -10,12 +12,19 @@ public class main {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        LoginParser parser = new LoginParser("b5310545620", "wairungtonas12", "0");
-        try {
-            parser.login();
-        } catch (IOException ex) {
-            System.out.println("Connection Failed to login");
+    public static void main(String[] args) throws IOException, InterruptedException {
+        LoginParser parser = new LoginParser("b5310XXXXXX", "password", "0");
+
+        while (true) {
+            try {
+                Thread.sleep(10000);
+                if (InternetChecker.getDefaultInstance().isConnectionPresent()) {
+                } else {
+                    parser.login();
+                }
+            } catch (Exception ex) {
+                System.out.println("Connection Failed to login");
+            }
         }
     }
 }
